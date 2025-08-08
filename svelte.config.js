e9e1446cde1e741c6655a882b06bf4e250b0bfae
Isaias Gorocica ELizondo <131029946+igsz0@users.noreply.github.com>
2025-08-08 10:18:10 -0600

    added vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,7 @@
-// svelte.config.js
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 
 export default {
   kit: {
-    adapter: adapter({
-      fallback: 'index.html'
-    }),
-    prerender: {
-      entries: ['*'] // importante para que exporte todas las rutas encontradas
-    }
+    adapter: adapter()
   }
 };
